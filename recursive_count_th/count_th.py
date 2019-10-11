6606d88d7cb9f72550f2f24a305b63d7ec70adbb
maxgunter99 <maxgunter99@gmail.com>
2019-10-11 11:38:12 -0500

    added recursive count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,56 @@
 '''
-Your function should take in a signle parameter (a string `word`)
+Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+    if word == '':
+        return 0
+
+    if len( word ) > 2:
+        word = [ word , 0 ]
+
+
+    if len( word[0] ) > 2:
+
+        if word[0][0] == 't':
+            print( 't' )
+            if word[0][1] == 'h':
+                print( 'h' )
+                
+                word[0] = word[0][1:]
+
+                if type( word[ 1 ] ) == int:
+                    word[ len( word ) - 1 ] += 1
+                    count_th( word )
+                else:
+                    word = [ word , 1 ]
+                    count_th( word )
+            else:
+                word[0] = word[0][1:]
+                count_th( word )
+
+        else:
+            word[0] = word[0][1:]
+            count_th( word )
+    else:
+        print( 'end of word' , word[1] )
+        if word[0][0] == 't':
+            if word[0][1] == 'h':
+                
+                word[0] = word[0][1:]
+
+                if type( word[ 1 ] ) == int:
+                    word[ len( word ) - 1 ] += 1
+                    count_th( word )
+                else:
+                    word = [ word , 1 ]
+                    count_th( word )
+        else:
+            return word[1]
+
+    print( word )
+    return word[1]
+
+count_th( "thhtthht")
